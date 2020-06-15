1eeb11e0ece32e7c246114a526121eff30821752
Georgy Shepelev <geo.shepelev@gmail.com>
2020-06-15 17:07:45 +0400

    issue rogii-com/cnpm#7
    tests for well known packages
    test for incorrect packages with non-digit versions (jpeg/openssl)
    test for packages with version = 0

---
+++ version0/wellKnownLinux/env.cmake
@@ -0,0 +1,988 @@
+NPM_ADD_PACKAGE(
+    NAME
+        compact_enc_det
+    VERSION
+        1.0.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        boost
+    VERSION
+        1.62.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qt
+    VERSION
+        5.9.6
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        3
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        osg
+    VERSION
+        3.4.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        protobuf
+    VERSION
+        2.6.1
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        "gxx9.2.1"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        boost
+    VERSION
+        1.68.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qt
+    VERSION
+        5.9.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        python
+    VERSION
+        3.7.4
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qt
+    VERSION
+        5.9.6
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        rapidjson
+    VERSION
+        1.1.0
+    ARCHITECTURE
+        anycpu
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gtest
+    VERSION
+        1.8.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        openssl
+    VERSION
+        1.0.2.15
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        osg
+    VERSION
+        3.4.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        osg
+    VERSION
+        3.4.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        "gxx9.2.1"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        boost
+    VERSION
+        1.72.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        openssl
+    VERSION
+        1.0.2.15
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        protobuf
+    VERSION
+        2.6.1
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qt
+    VERSION
+        5.9.9
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        compact_enc_det
+    VERSION
+        1.0.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "_g406291a"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        boost
+    VERSION
+        1.68.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        boost
+    VERSION
+        1.68.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        shapelib
+    VERSION
+        1.3.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        rapidjson
+    VERSION
+        1.1.0
+    ARCHITECTURE
+        anycpu
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qt
+    VERSION
+        5.9.6
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qt
+    VERSION
+        5.9.6
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        openssl
+    VERSION
+        1.0.2.15
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gxx_runtime
+    VERSION
+        9.2.1
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        cdumplib
+    VERSION
+        1.0.2
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "fake"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        protobuf
+    VERSION
+        2.6.1
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        nlohmann_json
+    VERSION
+        3.7.3
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.34
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        31
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.30
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.30
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.63
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        11
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.2.10
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        "_d0702d"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gridding
+    VERSION
+        2.5.22
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.30
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        4
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.23
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        protobuf
+    VERSION
+        2.6.1
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        3
+    TAG
+        "gxx9.2.1"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.36
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        40
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.2.10
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        witsml
+    VERSION
+        2.3.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.3.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        witsml
+    VERSION
+        2.1.10
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.32
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        29
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.2.8
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.3.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.63
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        10
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gridding
+    VERSION
+        2.5.22
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.35
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        36
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gridding
+    VERSION
+        2.5.17
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        googlemock
+    VERSION
+        1.10.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.46
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        5
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        ribbon
+    VERSION
+        4.13.5
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        3
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        libxl
+    VERSION
+        3.7.2.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        "fake"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        xlt
+    VERSION
+        0.6.12
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.58
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        9
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gridding
+    VERSION
+        2.6.2
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        gridding
+    VERSION
+        2.8.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.33
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        30
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.3.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "sdk18362"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        ribbon
+    VERSION
+        4.13.5
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.23
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        autogeosteering
+    VERSION
+        0.3.3
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.30
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        5
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        qtitanribbon
+    VERSION
+        4.18.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        xtensor
+    VERSION
+        0.21.4
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        ribbon
+    VERSION
+        4.13.5
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.30
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        1
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.53
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        6
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.35
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        37
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.30
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        3
+    TAG
+        ""
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.0.29
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "_a38685"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        libxl
+    VERSION
+        3.7.2.0
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        0
+    TAG
+        "fake"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        resistivity
+    VERSION
+        1.1.42
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
+NPM_ADD_PACKAGE(
+    NAME
+        ribbon
+    VERSION
+        4.13.5
+    ARCHITECTURE
+        amd64
+    BUILD_NUMBER
+        2
+    TAG
+        "gxx9.2.1_qt5.9.9"
+)
+
