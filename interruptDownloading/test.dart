32cc345036f8a46280e7d9d717f86e13949afcfd
Georgy Shepelev <geo.shepelev@gmail.com>
2020-06-16 18:30:45 +0400

    issue rogii-com/cnpm#11
    use Dart to implement the test

---
+++ interruptDownloading/test.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'dart:convert';
+
+enum State
+{
+    Initial,
+    Complete0PercentFound,
+    ProcessKilled,
+}
+
+const skipTestReturnCode = 254;
+
+void deleteIfExists(final Directory path)
+{
+    if (path.existsSync())
+        path.deleteSync(recursive: true);
+}
+
+Future main(final List<String> args) async
+{
+    assert(args.length > 4);
+
+    final currentDirectory = Directory.current;
+    deleteIfExists(Directory(currentDirectory.path + '/3rd_party'));
+
+    final process = await Process.start(args[0], [ '-G', args[1], '-DCMAKE_BUILD_TYPE=${args[2]}', '-DCNPM_SOURCE_ROOT=${args[3]}', args[4] ]);
+
+    var state = State.Initial;
+    await for (var data in process.stdout.transform(utf8.decoder))
+    {
+        print('======================= stdout new element start');
+        print(data);
+        print('======================= stdout new element end');
+
+        switch (state)
+        {
+            case State.Initial:
+                if (data.indexOf(RegExp(r'0%[ \t]+complete')) != -1)
+                    state = State.Complete0PercentFound;
+
+                break;
+
+            case State.Complete0PercentFound:
+                final killed = process.kill();
+                print(killed);
+                if (!killed)
+                {
+                    exitCode = skipTestReturnCode;
+                    return;
+                }
+
+                state = State.ProcessKilled;
+                break;
+
+            case State.ProcessKilled:
+                print('wait for finished');
+                if (data.indexOf(RegExp(r'after foreach;')) != -1)
+                {
+                    // looks like the package was downloaded successfully
+                    exitCode = skipTestReturnCode;
+                    return;
+                }
+                break;
+        }
+    }
+
+    final packagePath = currentDirectory.path + '/3rd_party/boost-1.72.0-amd64-1sdk18362_vsbt19.7z';
+    if (File(packagePath).existsSync())
+    {
+        print("'$packagePath' exists");
+        exitCode = 1;
+    }
+}
+
---
+++ interruptDownloading/test/packageInfo.cmake
@@ -0,0 +1,25 @@
+set(
+    PACKAGE_NAME
+    "boost"
+)
+
+set(
+    PACKAGE_VERSION
+    "1.72.0"
+)
+
+set(
+    PACKAGE_ARCHITECTURE
+    "amd64"
+)
+
+set(
+    PACKAGE_BUILD_NUMBER
+    1
+)
+
+set(
+    PACKAGE_TAG
+    "sdk18362_vsbt19"
+)
+
